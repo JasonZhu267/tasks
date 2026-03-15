40fef248481c99092ce72ee30b48564021a32786
Jason Zhu <jasonzhu@udel.edu>
2026-03-14 23:23:28 -0400

    completed mcq

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,32 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selectedOption, setSelectedOption] = useState<string>(options[0]);
+
+    function updateOption(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedOption(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <Form.Group controlId="selectedOption">
+                <Form.Label>How do you feel?</Form.Label>
+                <Form.Select value={selectedOption} onChange={updateOption}>
+                    {options.map((Option: string) => (
+                        <option key={Option} value={Option}>
+                            {Option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+
+            <div>
+                {selectedOption === expectedAnswer ?
+                    <span>✔️</span>
+                :   <span>❌</span>}
+            </div>
         </div>
     );
 }
